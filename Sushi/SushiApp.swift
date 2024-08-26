60378c0b2f08f2221e1fc5e86301e2f5ac6f84dc
DiegoRodriguez3D <69866476+DiegoRodriguez3D@users.noreply.github.com>
2024-08-26 11:34:34 +0200

    Initial Commit

---
+++ Sushi/SushiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiApp.swift
+//  Sushi
+//
+//  Created by Diego Rodriguez on 26/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SushiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
